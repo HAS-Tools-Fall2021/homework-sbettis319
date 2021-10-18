0b57a35771e54035756eed3ee5382bd5166e8cc0
Sierra Bettis <sbettis@email.arizona.edu>
2021-10-18 09:28:17 -0700

    Week 8 Homework Submission

---
+++ Forecast_Submissions/Bettis_HW8.md
@@ -0,0 +1,36 @@
+Sierra Bettis
+HW 8
+
+1. An explanation of how you generated your forecasts and why?
+   I generated my forecasts using the datetime and found the mean over a range of dates for each week. I did this so that I could get more of the values that I was looking for in my time frame.
+2. The three graphs you are including with an explanation of why you chose these three graphs.
+
+Graph 1: Flow in 2010-2020 from October 17-23
+I chose to alter this graph 3 times to look at each week and then both weeks together as a whole. 
+   ![picture 7](../../images/b2367fb5fc888b5369875572f0e66a3529d4a9929b726a922c3753a6c16aa5a6.png)  
+
+Graph 2: Flow in 2010-2020 from October 24-30
+Second iteration of the previous grapg, but for week 2.
+![![picture 3](/Users/sierra/Desktop/Desktop%20-%20Sierra%E2%80%99s%20MacBook%20Pro/Fall%202021/HASTools/images/b2367fb5fc888b5369875572f0e66a3529d4a9929b726a922c3753a6c16aa5a6.png)  
+ 1](../../images/c9460a89b01f96d6ea93ed31c583f27449b4b7100265d614704d181b32b49e65.png)  
+
+Graph 3: Flow in 2010-2020 from October 18-30th (both weeks together)
+Third iteration of the first graph, just seeing both weeks together and how the flow changes.
+![picture 8](../../images/ca00d03f257ca19c595e9a05eb09fba0c0b4943ef2b265d3f8c401dbf6c48ae2.png)  
+ 
+ Graph 4 : Box and Whisker plot showing the flow for everyday
+I wanted this graph just to see general flow everyday and how much each day can fluctuate. It shows me that the flow values become more uniform towards the end of each month. 
+![![picture 3](/Users/sierra/Desktop/Desktop%20-%20Sierra%E2%80%99s%20MacBook%20Pro/Fall%202021/HASTools/images/b2367fb5fc888b5369875572f0e66a3529d4a9929b726a922c3753a6c16aa5a6.png)  
+![![picture 3](/Users/sierra/Desktop/Desktop%20-%20Sierra%E2%80%99s%20MacBook%20Pro/Fall%202021/HASTools/images/b2367fb5fc888b5369875572f0e66a3529d4a9929b726a922c3753a6c16aa5a6.png)  
+ 1](../../images/c9460a89b01f96d6ea93ed31c583f27449b4b7100265d614704d181b32b49e65.png)  
+![![picture 3](/Users/sierra/Desktop/Desktop%20-%20Sierra%E2%80%99s%20MacBook%20Pro/Fall%202021/HASTools/images/b2367fb5fc888b5369875572f0e66a3529d4a9929b726a922c3753a6c16aa5a6.png)  
+![![picture 3](/Users/sierra/Desktop/Desktop%20-%20Sierra%E2%80%99s%20MacBook%20Pro/Fall%202021/HASTools/images/b2367fb5fc888b5369875572f0e66a3529d4a9929b726a922c3753a6c16aa5a6.png)  
+ 1](../../images/c9460a89b01f96d6ea93ed31c583f27449b4b7100265d614704d181b32b49e65.png)  
+ 1](../../images/ca00d03f257ca19c595e9a05eb09fba0c0b4943ef2b265d3f8c401dbf6c48ae2.png)  
+ 1](../../images/7166cb5e0683b2fca59343715c135cc20faf47ee3fdb6aef4ddae24603a7722f.png)  
+ 
+1. A brief summary of what you got out of the peer evaluation. How did you make your script better based on the feedback you received?
+   I did get some helpful comments on adding doc-strings and also editing my code more to make have it follow the PEP8 style more. I also did take the advice of automatically saving the graphs and also changing the initial name of the function to be different than the output. 
+2. A summary of how you are using timeseries functionality in your script.
+Describe the part of your script that you are most proud of and why.
+    I used the timeseries functionality to create my forecast for this week. I would say that I am most proud of my function and doc-string section.
---
+++ Forecast_Submissions/Bettis_HW8.py
@@ -0,0 +1,122 @@
+# %%
+# Import the modules we will use
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+# Set a grey background
+sns.set(style="darkgrid")
+df = sns.load_dataset('iris')
+# %%
+# Set the file name and path to where you have stored the data
+filename = 'streamflow_week8.txt'
+filepath = os.path.join('..', '..', 'data', filename)
+print(os.getcwd())
+print(filepath)
+
+# %%
+# Read in our data as a dataframe - making the dates into a datetime object
+data = pd.read_table(filepath, sep='\t', skiprows=30,
+                      names=['agency_cd', 'site_no',
+                              'datetime', 'flow', 'code'],
+                              parse_dates=['datetime'])
+
+# Expand the dates to year month day
+data['year'] = pd.DatetimeIndex(data['datetime']).year
+data['month'] = pd.DatetimeIndex(data['datetime']).month
+data['day'] = pd.DatetimeIndex(data['datetime']).day
+data['dayofweek'] = pd.DatetimeIndex(data['datetime']).dayofweek
+# %%
+# Setting datetime as the index
+datai = data.copy()
+datai = datai.set_index('datetime')
+datai.head()
+
+# %% Function with a for loop
+def function(month, daysofmonth, startyear, data):
+
+
+    '''This function can be used to find October means for any year.
+        Parameters
+        ----------
+        month: int
+                Input the month of flows desired.
+        daysofmonth: int
+                Input what day of the month you want to look at.
+        startyear: int
+                Input the year you want to start with.
+        data: int
+                This is important for the data to output.
+        Returns
+        ------
+        The October means for the month and year chosen.
+    '''
+oct_mean = np.zeros(31)
+
+
+for d in range(31):
+    daytemp = d+1
+    tempdata = data[(data['year'] >= 2016) & (data['month'] == 10) &
+        (data['day'] == daytemp)]
+    oct_mean[d] = np.mean(tempdata['flow'])
+    print('Iteration', d, 'Day=', daytemp, 'Flow=', oct_mean[d])
+
+function(10, 17, 2016, data)
+
+# %%
+# Finding my forecast submission values using datatime
+datai = datai[datai.index.month == 10]
+octmean1 = datai['2010-10-17':'2020-10-23'].mean()
+octmean2 = datai['2010-10-24':'2020-10-30'].mean()
+
+print("My forecast for week 1 is ", octmean1, "cfs")
+print("My forecast for week 2 is ", octmean2, "cfs")
+print("Flow seems to be larger for the second week forecast")
+# Just look at the flow values
+# %%
+# Graph 1: Flow in 2010-2020 from October 17-23
+fig, ax = plt.subplots()
+for i in range(2010, 2020):
+    plot_data = data[(data['year'] == i) & (data['month'] == 10)]
+    ax.plot(plot_data['day'], plot_data['flow'],
+    label=i)
+    ax.set(title="October Flow, 17-23", xlim=[17, 23], ylim=[50, 300])
+    ax.legend(ncol=2)
+plt.show()
+fig.savefig("Graph1.png")
+print("2010 had a an above average flow on the 22nd of the month")
+# %%
+# Graph 2: Flow in 2010-2020 from October 24-30
+fig, ax = plt.subplots()
+for i in range(2010, 2020):
+    plot_data = data[(data['year'] == i) & (data['month'] == 10)]
+    ax.plot(plot_data['day'], plot_data['flow'],
+    label=i)
+    ax.set(title="October Flow, 24-30", xlim=[24, 30], ylim=[50, 200])
+    ax.legend(ncol=2)
+plt.show()
+fig.savefig("Graph2.png")
+print("2011 had an above average flow from the 27-28th")
+# %%
+# Graph 3: Flow in 2010-2020 from October 18-30th (both weeks together)
+fig, ax = plt.subplots()
+for i in range(2010, 2020):
+    plot_data = data[(data['year'] == i) & (data['month'] == 10)]
+    ax.plot(plot_data['day'], plot_data['flow'],
+    label=i)
+    ax.set(title="October Flow, 17-30", xlim=[17, 30], ylim=[50, 260])
+    ax.legend(ncol=2)
+plt.show()
+fig.savefig("Graph3.png")
+print("Peaks in 2010 and 2015 might throw off the average flow values")
+# %%
+# Graph 4 : Box and Whisker plot showing the flow for everyday
+fig, ax = plt.subplots()
+ax = sns.boxplot(x='day', y="flow",  data=data, linewidth=0.1)
+ax.set(yscale='log')
+ax.set_xlabel('Forecast Week')
+ax.set_ylabel('Flow (cfs)')
+plt.show()
+fig.savefig("Graph4.png")
